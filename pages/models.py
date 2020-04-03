509af090cc480ec8ed367d67d52afca463a87d64
Samiksha Agrawal <agrawalsamiksha25@gmail.com>
2020-04-04 02:16:54 +0530

    erroredit

--- pages/models.py
+++ pages/models.py
@@ -3,44 +3,44 @@ from django.db import models
 # Create your models here.
 class allos(models.Model):
     item1 = models.CharField(max_length=100,null=True)
-    item2 = models.CharField(max_length=100,null=True)
-    item3 = models.CharField(max_length=100,null=True)
-    item4 = models.CharField(max_length=100,null=True)
+    item2 = models.CharField(max_length=100,null=True,blank=True)
+    item3 = models.CharField(max_length=100,null=True,blank=True)
+    item4 = models.CharField(max_length=100,null=True,blank=True)
 
 class ayurs(models.Model):
     item1 = models.CharField(max_length=100,null=True)
-    item2 = models.CharField(max_length=100,null=True)
-    item3 = models.CharField(max_length=100,null=True)
-    item4 = models.CharField(max_length=100,null=True)
+    item2 = models.CharField(max_length=100,null=True,blank=True)
+    item3 = models.CharField(max_length=100,null=True,blank=True)
+    item4 = models.CharField(max_length=100,null=True,blank=True)
 
 class disps(models.Model):
     item1 = models.CharField(max_length=100,null=True)
-    item2 = models.CharField(max_length=100,null=True)
-    item3 = models.CharField(max_length=100,null=True)
-    item4 = models.CharField(max_length=100,null=True)
+    item2 = models.CharField(max_length=100,null=True,blank=True)
+    item3 = models.CharField(max_length=100,null=True,blank=True)
+    item4 = models.CharField(max_length=100,null=True,blank=True)
 
 class neuts(models.Model):
     item1 = models.CharField(max_length=100,null=True)
-    item2 = models.CharField(max_length=100,null=True)
-    item3 = models.CharField(max_length=100,null=True)
-    item4 = models.CharField(max_length=100,null=True)
+    item2 = models.CharField(max_length=100,null=True,blank=True)
+    item3 = models.CharField(max_length=100,null=True,blank=True)
+    item4 = models.CharField(max_length=100,null=True,blank=True)
 
 class pharms(models.Model):
     item1 = models.CharField(max_length=100,null=True)
-    item2 = models.CharField(max_length=100,null=True)
-    item3 = models.CharField(max_length=100,null=True)
-    item4 = models.CharField(max_length=100,null=True)
+    item2 = models.CharField(max_length=100,null=True,blank=True)
+    item3 = models.CharField(max_length=100,null=True,blank=True)
+    item4 = models.CharField(max_length=100,null=True,blank=True)
 
 class respis(models.Model):
     item1 = models.CharField(max_length=100,null=True)
-    item2 = models.CharField(max_length=100,null=True)
-    item3 = models.CharField(max_length=100,null=True)
-    item4 = models.CharField(max_length=100,null=True)
+    item2 = models.CharField(max_length=100,null=True,blank=True)
+    item3 = models.CharField(max_length=100,null=True,blank=True)
+    item4 = models.CharField(max_length=100,null=True,blank=True)
 
 class surgis(models.Model):
     item1 = models.CharField(max_length=100,null=True)
-    item2 = models.CharField(max_length=100,null=True)
-    item3 = models.CharField(max_length=100,null=True)
-    item4 = models.CharField(max_length=100,null=True)
+    item2 = models.CharField(max_length=100,null=True,blank=True)
+    item3 = models.CharField(max_length=100,null=True,blank=True)
+    item4 = models.CharField(max_length=100,null=True,blank=True)
 
     
