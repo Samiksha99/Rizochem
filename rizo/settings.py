181be9f6ffacfaf583eb449348e1683bdfb70f37
Samiksha Agrawal <agrawalsamiksha25@gmail.com>
2020-04-03 22:23:44 +0530

    'ayurallodone'

--- rizo/settings.py
+++ rizo/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'rizo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'rizochem',
+        'USER':'postgres',
+        'PASSWORD':'quad_sas123',
+        'HOST':'localhost' 
     }
 }
 
