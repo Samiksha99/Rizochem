ce5f7f45bbab16e2b94a987aafe75eb2413b2db7
Samiksha Agrawal <agrawalsamiksha25@gmail.com>
2020-04-20 20:55:18 +0530

    carosuel not working

--- rizochem/urls.py
+++ rizochem/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.index, name='index')
+    path('',views.index, name='index'),
+    path('',views.contactus, name="contactus")
    
 ]
--- rizochem/views.py
+++ rizochem/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
 
 def index(request):
     return render(request, "index.html")
+
+def contactus(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        query = request.POST['query']
+
+        user = User.objects.create_user(name=name, email=email, query=query)
+        user.save();
+        print('Query sent')
+        return redirect('/')
+
+    else:
+        return render(request, "index.html")
+
